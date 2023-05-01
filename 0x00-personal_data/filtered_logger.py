29e13e028c7d3f94eac9b5d90ca99101884b07c4
apha-pesso <mutiuayo99@gmail.com>
2023-05-01 22:16:21 +0100

    COnnect to database

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,6 +3,8 @@
 from typing import List
 import re
 import logging
+import os
+import mysql.connector
 
 PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
 
@@ -57,3 +59,20 @@ def get_logger() -> logging.Logger:
     handler.setFormatter(formatter)
     logger.addHandler(handler)
     return (logger)
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    '''Connect to db'''
+    user = os.getenv('PERSONAL_DATA_DB_USERNAME', 'root')
+    password = os.getenv('PERSONAL_DATA_DB_PASSWORD', '')
+    host = os.getenv('PERSONAL_DATA_DB_HOST', 'localhost')
+    db_name = os.getenv('PERSONAL_DATA_DB_NAME')
+
+    # Connect to database
+    conn = mysql.connector.connect(
+        user=user
+        password=password
+        host=host
+        database=db_name
+    )
+    return (conn)
