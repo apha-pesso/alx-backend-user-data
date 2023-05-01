e18031e349e65a3614e3e82d11fa8984f5292988
apha-pesso <mutiuayo99@gmail.com>
2023-05-01 23:15:51 +0100

    Validate encrypted password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -8,3 +8,9 @@ def hash_password(password: str) -> bytes:
     hack = bcrypt.gensalt()
     hashed_pw = bcrypt.hashpw(password.encode('utf-8'), hack)
     return hashed_pw
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    '''Validate password'''
+    check = bcrypt.checkpw(password.encode('utf-8'), hashed_password)
+    return (check)
