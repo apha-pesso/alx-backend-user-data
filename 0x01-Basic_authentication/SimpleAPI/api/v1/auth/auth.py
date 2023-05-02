8771512232332ffcd5b6d4bffc47035749e13264
apha-pesso <mutiuayo99@gmail.com>
2023-05-02 17:19:45 +0100

    Push files

---
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/auth/auth.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+'''Auth class'''
+
+from flask import request
+from typing import List, TypeVar
+
+class Auth():
+    '''Auth class'''
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        '''require auth'''
+        if path is None:
+            return True
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        for excluded_path in excluded_paths:
+            if path.rstrip('/').startswith(excluded_path.rstrip('/')):
+                return False
+        return True
+        # if not path or excluded_paths is None:
+            # return True
+
+    '''
+        for p in excluded_paths:
+            if path == p:
+                return False
+            else:
+                return True
+        for excluded_path in excluded_paths:
+            if path.rstrip('/').startswith(excluded_path.rstrip('/')):
+                return False
+
+        # if path in excluded_paths:
+            return False
+
+        # return False
+    '''
+
+    def authorization_header(self, request=None) -> str:
+        '''Authorization Header'''
+        # if request is None or request.headers('Authorization') is None:
+        if request is None or 'Authorization' not in request.headers:
+            return None
+        return request.headers['Authorization']
+
+    def current_user(self, request=None) -> TypeVar('User'):
+    # def current_user(self, request=None):
+        '''Current user'''
+        return None
