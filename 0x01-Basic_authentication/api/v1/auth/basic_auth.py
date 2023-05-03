a3a0c00a93afda65416611a6c544ce53ada98038
apha-pesso <mutiuayo99@gmail.com>
2023-05-03 10:24:51 +0100

    Get User object

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,8 @@
 
 from api.v1.auth.auth import Auth
 import base64
+from models.user import User
+from typing import TypeVar
 
 
 class BasicAuth(Auth):
@@ -50,3 +52,22 @@ class BasicAuth(Auth):
 
         if len(cred) == 2:
             return (cred[0], cred[1])
+
+    def user_object_from_credentials(
+            self,
+            user_email: str,
+            user_pwd: str) -> TypeVar('User'):
+        '''Get user object'''
+        if not user_email or not isinstance(user_email, str):
+            return None
+        if not user_pwd or not isinstance(user_pwd, str):
+            return None
+
+        user = User.search({'email': user_email})
+        if len(user) == 0:
+            return None
+
+        if user[0].is_valid_password(user_pwd):
+            return user[0]
+        else:
+            return None
