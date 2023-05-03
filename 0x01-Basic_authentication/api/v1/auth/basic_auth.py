8163e0e5a56bd52b622316b4296126b97055f73f
apha-pesso <mutiuayo99@gmail.com>
2023-05-03 08:52:00 +0100

    Authorization check

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,4 +6,15 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     '''Basic Auth class'''
-    pass
+
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        '''Extract Authorization header'''
+        if not authorization_header or not isinstance(
+                authorization_header,
+                str) or not authorization_header.startswith('Basic'):
+            return None
+        authy = authorization_header.split()
+        if len(authy) < 2:
+            return None
+        return authy[1]
