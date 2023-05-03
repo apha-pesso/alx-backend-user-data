e7d7b61e5c8cebe2ec02a3ef536c01cf44c0b176
apha-pesso <mutiuayo99@gmail.com>
2023-05-03 09:08:53 +0100

    Decode Authy

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 '''Basic Auth class'''
 
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -18,3 +19,16 @@ class BasicAuth(Auth):
         if len(authy) < 2:
             return None
         return authy[1]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        '''Decode base64 string'''
+        if not base64_authorization_header or not isinstance(
+                base64_authorization_header, str):
+            return None
+
+        try:
+            decoded = base64.b64decode(base64_authorization_header)
+            return decoded.decode('utf-8')
+        except BaseException:
+            return None
