8e4c9bc695667ab7ee3ab8a88c5456471075b116
apha-pesso <mutiuayo99@gmail.com>
2023-05-04 22:13:50 +0100

    Implement checks before request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -21,6 +21,7 @@ auth = auth_type
 
 excluded_paths = [
     '/api/v1/status/',
+    '/api/v1/auth_session/login/',
     '/api/v1/unauthorized/',
     '/api/v1/forbidden/']
 
@@ -44,6 +45,9 @@ def before_request_func():
         # print(path)
         if auth.require_auth(path, excluded_paths):
             if auth.authorization_header(request) is None:
+                abort(401)
+            if not auth.authorization_header(
+                    request) and not auth.session_cookie(request):
                 abort(401)
             if auth.current_user(request) is None:
                 abort(403)
