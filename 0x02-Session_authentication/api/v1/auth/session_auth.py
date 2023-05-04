a1b44ae19ea4bd4bd1420e48ed687b87b32549ea
apha-pesso <mutiuayo99@gmail.com>
2023-05-04 12:20:42 +0100

    Session ID

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,6 +2,7 @@
 '''Session Auth class'''
 
 from api.v1.auth.auth import Auth
+from uuid import uuid4
 # from models.user import User
 # from typing import TypeVar
 
@@ -9,4 +10,11 @@ from api.v1.auth.auth import Auth
 class SessionAuth(Auth):
     '''Session Auth class'''
     user_id_by_session_id = {}
-    pass
+
+    def create_session(self, user_id: str = None) -> str:
+        '''Create session id'''
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        session_id = str(uuid4())
+        user_id_by_session_id[session_id] = user_id
+        return session_id
