b36aaa43960ecf8463d4a8c3101163990d4b4dca
apha-pesso <mutiuayo99@gmail.com>
2023-05-04 21:17:55 +0100

    Return user_id for a session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -11,6 +11,12 @@ class SessionAuth(Auth):
     '''Session Auth class'''
     user_id_by_session_id = {}
 
+    def __init__(self):
+        '''Initializer'''
+        super().__init__()
+
+        # self.user_id_by_session_id = user_id_by_session_id
+
     def create_session(self, user_id: str = None) -> str:
         '''Create session id'''
         if user_id is None or not isinstance(user_id, str):
@@ -18,3 +24,11 @@ class SessionAuth(Auth):
         session_id = str(uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        '''Return user id'''
+        if session_id is None or not isinstance(session_id, str):
+            return None
+
+        user_id = self.user_id_by_session_id.get(session_id)
+        return user_id
