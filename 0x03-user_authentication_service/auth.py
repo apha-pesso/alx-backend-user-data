477d08d686d5c3b680a7b6285e1270bfd108c1ac
apha-pesso <mutiuayo99@gmail.com>
2023-05-10 14:28:23 +0100

    Test again

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,17 +1,24 @@
 #!/usr/bin/env python3
-'''Authy'''
+""""Password hashing
+"""
+from bcrypt import hashpw, gensalt
 import bcrypt
-from db import DB
+import base64
+from db import DB, User
+from typing import Optional
+from sqlalchemy.orm.exc import NoResultFound
 from user import User
+from uuid import uuid4
 
 
-def _hash_password(password: str) -> bytes:
-    '''Hash password and return byte'''
-
-    salt = bcrypt.gensalt()
-
-    hashed = bcrypt.hashpw(password.encode('utf-8'), salt)
-    return hashed
+def _hash_password(self, password: str) -> bytes:
+    """Returns encrypted password
+    Args: password
+    """
+    # salt = bcrypt.gensalt()
+    # encoded_password = password.encode('utf-8')
+    # hashed_password = bcrypt.hashpw(encoded_password, salt)
+    return hashpw(password.encode('utf-8'), gensalt())
 
 
 class Auth:
@@ -22,13 +29,19 @@ class Auth:
         self._db = DB()
 
     def register_user(self, email: str, password: str) -> User:
-        '''Register User class'''
-        exist = self._db.find_user_by(email=email)
-        if exist:
-            raise ValueError(f'User {email} already exists')
+        """Create a new User by given Email and password
+            if user exists with a given email return:
+                User <user's email> already exists
 
-        hashed_password = _hash_password(password)
-        user = User(email=email, hashed_password=hashed_password)
-        self._db._session.add(user)
-        self._db._session.commit()
-        return user
+        """
+        # checking if user with email exists
+        try:
+            user = self._db.find_user_by(email=email)
+            if user:
+                raise ValueError('User {} already exists'.format(email))
+        except NoResultFound:
+            # hashed the password
+
+            hashed_password = _hash_password(self, password)
+            new_user = self._db.add_user(email, hashed_password)
+            return new_user
