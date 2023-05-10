106ee85ba389396b65c3c1dac1b1306688984859
apha-pesso <mutiuayo99@gmail.com>
2023-05-10 14:50:11 +0100

    Checki

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -23,8 +23,7 @@ class Auth:
         self._db = DB()
 
     def register_user(self, email: str, password: str) -> User:
-        '''Register User class'''
-        '''
+        '''Register User class
         try:
             user = self._db.find_user_by(email=email)
             if user:
