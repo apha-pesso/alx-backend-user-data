b4afb202597aac2673f4bb378c0fd0f0d4c22f4f
apha-pesso <mutiuayo99@gmail.com>
2023-05-10 10:45:23 +0100

    Test12

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python3
-
-"""
-DB module
-"""
+"""DB module."""
 from sqlalchemy import create_engine
 from sqlalchemy.exc import InvalidRequestError, NoResultFound
 from sqlalchemy.ext.declarative import declarative_base
@@ -12,14 +9,10 @@ from user import Base, User
 
 
 class DB:
-    """
-    DB class
-    """
+    """DB class."""
 
     def __init__(self) -> None:
-        """
-        initialize a new DB instance
-        """
+        """Initialize a new DB instance."""
         self._engine = create_engine("sqlite:///a.db")
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
@@ -27,41 +20,34 @@ class DB:
 
     @property
     def _session(self) -> Session:
-        """
-        memoized session object
-        args:   self
-        return: self.__session: Session
-        """
+        """Memoized session object."""
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """
-        adds a user to the database
-        args:   self
-                email: str
-                hashed_password: str
-        return: user: User
-        """
+        """Create user."""
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
         return user
 
     def find_user_by(self, **kwargs) -> User:
-        """
-        finds the first user in the database that matches the given criteria
-        args:   self
-                **kwargs: arbitrary keyword arguments for filtering the query
-        return: user: User
-        """
-        try:
-            users = self._session.query(User)
-            user: User = users.filter_by(**kwargs).one()
-        except NoResultFound:
-            raise
-        except InvalidRequestError as e:
-            raise InvalidRequestError(str(e))
+        """Find user by keyword argument."""
+        if not kwargs:
+            raise InvalidRequestError
+        user = self._session.query(User).filter_by(**kwargs).first()
+        if user is None:
+            raise NoResultFound
         return user
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """Update user."""
+        user = self.find_user_by(id=user_id)
+        for key, value in kwargs.items():
+            if not hasattr(user, key):
+                raise ValueError
+            setattr(user, key, value)
+        self._session.commit()
+        return None
--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,27 +1,14 @@
 #!/usr/bin/env python3
-
+"""Model User
 """
-module for SQLAlchemy model `User` for a database table named `users`
-"""
-
-from bcrypt import gensalt, hashpw
-from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from typing import Optional
+from sqlalchemy import Column, String, Integer
 
 Base = declarative_base()
 
 
 class User(Base):
-    """
-    class for SQLAlchemy model `User`
-    attributes:
-        id, primary key: Column[int]
-        email, a non-nullable string: Column[str]
-        hashed_password, a non-nullable string: Column[str]
-        session_id, a nullable string: Column[str], optional
-        reset_token, a nullable string: Column[str], optional
-    """
+    """class User"""
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
@@ -29,11 +16,3 @@ class User(Base):
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
     reset_token = Column(String(250), nullable=True)
-
-    def __init__(self, email: str, hashed_password: str,
-                 session_id: Optional[str] = None,
-                 reset_token: Optional[str] = None) -> None:
-        self.email = email
-        self.hashed_password = hashed_password
-        self.session_id = session_id
-        self.reset_token = reset_token
