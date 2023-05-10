aa52975b9e8226df734ccaf21d3b7b593efc33b4
apha-pesso <mutiuayo99@gmail.com>
2023-05-10 22:30:42 +0100

    Create user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 '''Basic flask'''
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from sqlalchemy import except_
+from auth import Auth
 
+AUTH = Auth()
 # Create a flask instance
 app = Flask(__name__)
 
@@ -12,6 +15,24 @@ def first_route():
     '''Return a message'''
     return jsonify({"message": "Bienvenue"})
 
+# Endpoint to register user
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users():
+    '''Endpoint to register user'''
+    data = request.form
+    # data = request.get_json()
+    email = data.get('email')
+    password = data.get('password')
+
+    try:
+        AUTH.register_user(email, password)
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+
+    return jsonify({"email": f"{email}", "message": "user created"})
+
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+    app.run(host="0.0.0.0", port="5000", debug=True)
