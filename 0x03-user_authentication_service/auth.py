1b93a46786e69b7e5707340f0ec76a22ce4dac9d
apha-pesso <mutiuayo99@gmail.com>
2023-05-10 23:04:39 +0100

    Validate password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -39,3 +39,15 @@ class Auth:
             hashed_password = _hash_password(password)
             new_user = self._db.add_user(email, hashed_password)
             return new_user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        '''Validate User password'''
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return False
+
+        hashed_pw = user.hashed_password
+        input_byte = password.encode('utf-8')
+
+        return bcrypt.checkpw(input_byte, hashed_pw)
