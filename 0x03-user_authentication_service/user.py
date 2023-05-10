a34b8117b6d266158ceedfbac1bcea933c89faeb
apha-pesso <mutiuayo99@gmail.com>
2023-05-10 08:27:40 +0100

    Add readme and user file

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+'''
+Create SQLAlchemy model named User with attributes
+'''
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+# create the user class
+
+
+class User(Base):
+    '''Creating the user table'''
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
