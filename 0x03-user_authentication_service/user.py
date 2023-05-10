e6dd741ac3601df0a4abdc13de4b8d804c9a727a
apha-pesso <mutiuayo99@gmail.com>
2023-05-10 08:52:02 +0100

    Inititalize method

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -5,6 +5,7 @@ Create SQLAlchemy model named User with attributes
 
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
+from typing import Optional
 
 Base = declarative_base()
 
@@ -19,3 +20,12 @@ class User(Base):
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
     reset_token = Column(String(250), nullable=True)
+
+    def __init__(self, email: str, hashed_password: str,
+                 session_id: Optional[str] = None,
+                 reset_token: Optional[str] = None) -> None:
+        '''Class constructor'''
+        self.email = email
+        self.hashed_password = hashed_password
+        self.session_id = session_id
+        self.reset_token = reset_token
