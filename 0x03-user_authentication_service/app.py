a19d078e47cb9855bfe2158e98b8f0516e2b7584
apha-pesso <mutiuayo99@gmail.com>
2023-05-11 10:40:06 +0100

    Create session

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 '''Basic flask'''
-from flask import Flask, jsonify, request
+from flask import Flask, abort, jsonify, make_response, request
 from sqlalchemy import except_
 from auth import Auth
 
@@ -32,6 +32,27 @@ def users():
         return jsonify({"message": "email already registered"}), 400
 
     return jsonify({"email": f"{email}", "message": "user created"})
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login():
+    '''Login function'''
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    # check password and email validity
+    if AUTH.valid_login(email, password):
+        # create a session_id with the email
+        session_id = AUTH.create_session(email)
+
+        # store session_id in cookie
+        response = make_response('response')
+        response.set_cookie("session_id", session_id)
+
+        # return json
+        return jsonify({"email": f"{email}", "message": "logged in"})
+    else:
+        abort(401)
 
 
 if __name__ == "__main__":
